2cc4f5f6c192d401e23d569ab751ae3640f8e15e
vedoctor <mico.mg52@gmail.com>
2021-05-18 17:54:04 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import pgzrun, sys
+
+WIDTH = 500
+HEIGHT = 500
+
+
+def generate_snake():
+
+
+
+def draw():
+    screen.fill('black')
+
+
+def update():
+    pass
+
+
+def on_key_down(key):
+    if key == keys.ESCAPE:
+        sys.exit()
+
+
+pgzrun.go()
